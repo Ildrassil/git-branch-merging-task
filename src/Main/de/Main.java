4bdefbd1f33c52fab2868f3cad45cd33e093ec0a
Ildrassil <schneiderjakob7@googlemail.com>
2023-12-14 12:06:35 +0100

    my Name is Jakob

---
+++ src/Main/de/Main.java
@@ -0,0 +1,17 @@
+package Main.de;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> nameSpelling = new ArrayList<>();
+        String name = "Jakob Schneider";
+        nameSpelling = Arrays.stream(name.split("")).toList();
+        nameSpelling.stream().forEach(System.out::println);
+
+    }
+
+
+}
